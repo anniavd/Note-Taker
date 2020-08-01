e309f212b245beea55f56844c877e71a77c4bcaa
annia valdes <millerl.mb79@gmail.com>
2020-08-01 02:13:12 -0500

    add a comments to the code

--- routes/apiRoutes/noteRouter.js
+++ routes/apiRoutes/noteRouter.js
@@ -2,7 +2,7 @@
 const router=require('express').Router();
 const noteController=require('../../controller/noteController');
 
-
+//call the functions from the notecontroller the GET,POST and DELETE
 router.route('/')
 .get(noteController.getNote)
 .post(noteController.createNote)
