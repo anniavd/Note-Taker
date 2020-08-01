31b9d616b7627b753ed1ce66ea7dcb9f074376a7
annia valdes <millerl.mb79@gmail.com>
2020-08-01 02:17:05 -0500

    add a comments to the code

--- controller/noteController.js
+++ controller/noteController.js
@@ -7,6 +7,7 @@ const path = require('path')
 
 
 module.exports = {
+  //show all the data from db 
   getNote: (req, res) => {
     fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', function (error, data) {
       console.log("Data from getnote " + data)
@@ -14,25 +15,27 @@ module.exports = {
 
     })
   },
+  // create a note for add to the api db
   createNote: (req, res) => {
     fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', function (error, data) {
       if (error) {
         throw new Error(error)
       }
-      var dbnote = JSON.parse(data);
-      console.log("Data from createnotes " + data)
-
+      var dbnote = JSON.parse(data);     
+      //create a new id for assigner to the items inside the api db
       var newId = dbnote[dbnote.length - 1].id + 1
+      //create the new note for add
       var newNote = { "title": req.body.title, "text": req.body.text, "id": newId };
       dbnote.push(newNote)
       console.log('nueva nota', newNote)
-
+     //write the new api bb 
       fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(dbnote), function (error, data) {
         res.json("Done")
       })
 
     })
   },
+  //delete a note from the api db
   deleteNote: (req, res) => {
     let id = req.params.id;
     fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', function (error, data) {
